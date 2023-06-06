1fbef0063a33768028fe70333bbd048db457208d
smartraysam <raysamtob@gmail.com>
2023-06-06 21:02:34 +0100

    Task 0 to Task 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if num < 99:
-        print(f"{ num:02 }", end=", ")
+        print(f"{num:02}", end=", ")
     else:
-        print(f"{ num }")
+        print(f"{num}")
