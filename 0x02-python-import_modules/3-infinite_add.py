1c8a8f5729c36791af1695b9b79c72e7622cee2e
smartraysam <raysamtob@gmail.com>
2023-06-08 18:59:44 +0100

    Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    sums  = 0
+    for i in range(len(sys.argv) - 1):
+        sums += int(sys.argv[i + 1])
+    print("{}".format(total))
