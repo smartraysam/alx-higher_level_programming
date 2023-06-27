bc6e93cad465cf7f1277358c9782bc4255a4b2c6
smartraysam <raysamtob@gmail.com>
2023-06-27 20:07:41 +0100

    Task 0 update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     n = 0
     for element in range(x):
@@ -9,3 +10,4 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return(n)
+
