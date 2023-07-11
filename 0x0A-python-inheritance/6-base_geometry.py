02f40aba15a996686bc1ae01a55337221814d680
smartraysam <raysamtob@gmail.com>
2023-07-11 13:12:56 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""6. Improve Geometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry based on task 5"""
+    
+    def area(self):
+        """Function that caclculate the area"""
+
+        raise Exception("area()  is not implemented")
