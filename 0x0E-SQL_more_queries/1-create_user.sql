14ba587fbba6ea2f08a72e53c94b39d8d7459894
smartraysam <raysamtob@gmail.com>
2023-08-17 20:33:12 +0100

    update task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,9 @@
 -- A script that creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' LIMIT 1;
+
+IF ROW_COUNT() = 0 THEN
+    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
+END IF;
+
