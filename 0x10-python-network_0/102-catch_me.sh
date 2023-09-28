0eb28230ca21d3ed366bfcf5bc5a2652ce2bf1f3
smartraysam <raysamtob@gmail.com>
2023-09-28 22:06:46 +0100

    Task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes reuest that causes the server to respond with message
-curl -s -L -X POST -H "User-Agent: You got me!" -d "param=value" 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -H "User-Agent: You got me!"  0.0.0.0:5000/catch_me
